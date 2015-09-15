824f97a7dd2b89cfbddc19d4c88d5031a8744299
Den <gottago@i.ua>
2015-09-16 00:05:59 +0300

    F- commit

--- UCITY-1 rename f.py
+++ UCITY-1 rename f.py
@@ -1,10 +1,10 @@
 import os
 def rename_files():
     # (1) get file names from a folder
-    file_list = os.listdir(r"E:\XCODE\PYTHON\prank")
+    file_list = os.listdir(r"E:\XCODE\PY2\prank")
     #print(file_list)
     saved_path = os.getcwd()
-    print("Good working 555GIT"+saved_path)
+    print("NOXXX___http://www.yslow.org http://gtmetrix.com/ dynaTrace AJAX"+saved_path)
     os.chdir(r"E:\XCODE\PYTHON\prank")
     #(2) for each file, rename filename
     for file_name in file_list:
